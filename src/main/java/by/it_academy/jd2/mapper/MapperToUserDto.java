cf7799e55d55b900c0edd87b2acd62fb349244bc
Peniaz Evgenii <evgenii.peniaz@gmail.com>
2024-08-24 15:25:13 +0300

    correct mapper for userDto, create userDto

---
+++ src/main/java/by/it_academy/jd2/mapper/MapperToUserDto.java
@@ -0,0 +1,29 @@
+package by.it_academy.jd2.mapper;
+
+import by.it_academy.jd2.dto.UserDto;
+import by.it_academy.jd2.entity.ERole;
+import by.it_academy.jd2.entity.UserEntity;
+import by.it_academy.jd2.mapper.api.IMapper;
+
+public class MapperToUserDto implements IMapper<UserEntity, UserDto>{
+    public static final IMapper<UserEntity, UserDto> INSTANCE = new MapperToUserDto();
+
+    private MapperToUserDto() {}
+
+    @Override
+    public UserDto mapFrom(UserEntity userEntity) {
+        return UserDto.builder()
+                .setId(userEntity.getId())
+                .setLogin(userEntity.getLogin())
+                .setPassword(userEntity.getPassword())
+                .setName(userEntity.getName())
+                .setBirthDate(userEntity.getBirthDate())
+                .setRegistrationDate(userEntity.getRegistrationDate())
+                .setRole(ERole.getRoleByName(userEntity.getRole()).orElse(null))
+                .build();
+    }
+
+    public static IMapper<UserEntity, UserDto> getInstance() {
+        return INSTANCE;
+    }
+}
