ba7951a0c803a759414e92574732f09551a035ac
Peniaz Evgenii <evgenii.peniaz@gmail.com>
2024-10-12 18:47:35 +0300

    create DaoException filter

---
+++ src/main/java/by/it_academy/jd2/controller/filter/DaoExceptionFilter.java
@@ -0,0 +1,23 @@
+package by.it_academy.jd2.controller.filter;
+
+
+import by.it_academy.jd2.dao.exception.DaoException;
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebFilter("/*")
+public class DaoExceptionFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        try {
+            chain.doFilter(request, response);
+        } catch (DaoException e) {
+            HttpServletResponse resp = (HttpServletResponse) response;
+            resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
+                    "Произошла ошибка при работе с базой данных. " + e.getMessage());
+        }
+    }
+}
